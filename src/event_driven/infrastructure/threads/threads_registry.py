6cb03a605a1b043a0c3d9726f673c9afe0bff6fa
agrubio <a.grubio@hotmail.com>
2026-08-27 14:36:37 +0200

    fix: added producer worker CLI

--- src/event_driven/infrastructure/threads/threads_registry.py
+++ src/event_driven/infrastructure/threads/threads_registry.py
@@ -2,7 +2,7 @@ from typing import Type
 
 from event_driven.infrastructure.config.enumerations import ThreadsEnum
 from event_driven.infrastructure.threads import BaseWorkerThread, InventoryThread, NotificationThread, OrderThread, \
-    PaymentThread
+    PaymentThread, ProducerThread
 
 # ------------------------------------------------------------------
 # REGISTRIES & ENUMS (Easily extensible to add more workers/brokers)
@@ -14,4 +14,5 @@ THREAD_REGISTRY: dict[ThreadsEnum, Type[BaseWorkerThread]] = {
     ThreadsEnum.NOTIFICATION: NotificationThread,
     ThreadsEnum.ORDER: OrderThread,
     ThreadsEnum.PAYMENT: PaymentThread,
+    ThreadsEnum.PRODUCER: ProducerThread,
 }
